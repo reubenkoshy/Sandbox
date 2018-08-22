d61dbbb2364707d98319a6db4444c14cc166242c
reubenkoshy <reuben.koshy@my.jcu.edu.au>
2018-08-22 18:54:14 +1000

    Added Password Check Program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,12 @@
 """Reuben Koshy"""
+
+password = input("Enter a Password: ")
+
+while len(password) >= 0:
+    if len(password) >= 8:
+        print("Valid Password")
+        print('*' * len(password))
+        break
+    else:
+        print("Invalid password. Must be at least 8 characters in length")
+        password = input("Enter a Password: ")
